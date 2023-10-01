ce02a37f84962aab873b94d4743ba4571c91745e
ProMinato500 <nandip500@gmail.com>
2023-10-01 14:35:51 +0530

    completed

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from "@components/Feed";
+import React from "react";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+      </h1>
+      <h1 className="orange_gradient head_text text-center">
+        AI-Powered Prompts
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
